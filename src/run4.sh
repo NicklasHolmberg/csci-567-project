964ea401bd767d49b24a1ede4683490a41be9169
Jeongsik Park <lucas.jeongsik.park@gmail.com>
2024-12-09 18:35:45 -0800

    divide experiments

---
+++ src/run4.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# # init method
+# python run_model.py --random_seed 42 --init_method random --augment 1 --skip_connection no
+# python run_model.py --random_seed 42 --init_method xavier --augment 1 --skip_connection no
+# python run_model.py --random_seed 42 --init_method he --augment 1 --skip_connection no
+# python run_model.py --random_seed 1 --init_method random --augment 1 --skip_connection no
+# python run_model.py --random_seed 1 --init_method xavier --augment 1 --skip_connection no
+# python run_model.py --random_seed 1 --init_method he --augment 1 --skip_connection no
+# python run_model.py --random_seed 99 --init_method random --augment 1 --skip_connection no
+# python run_model.py --random_seed 99 --init_method xavier --augment 1 --skip_connection no
+# python run_model.py --random_seed 99 --init_method he --augment 1 --skip_connection no
+# augment method
+# # python run_model.py --random_seed 42 --init_method random --augment 1 --skip_connection no #redundant
+# python run_model.py --random_seed 42 --init_method random --augment 2 --skip_connection no
+# python run_model.py --random_seed 42 --init_method random --augment 3 --skip_connection no
+# python run_model.py --random_seed 42 --init_method random --augment 4 --skip_connection no
+# python run_model.py --random_seed 42 --init_method random --augment 5 --skip_connection no
+# python run_model.py --random_seed 42 --init_method random --augment 6 --skip_connection no
+# python run_model.py --random_seed 42 --init_method random --augment 7 --skip_connection no
+# # python run_model.py --random_seed 1 --init_method random --augment 1 --skip_connection no #redundant
+# python run_model.py --random_seed 1 --init_method random --augment 2 --skip_connection no
+# python run_model.py --random_seed 1 --init_method random --augment 3 --skip_connection no
+# python run_model.py --random_seed 1 --init_method random --augment 4 --skip_connection no
+# python run_model.py --random_seed 1 --init_method random --augment 5 --skip_connection no
+# python run_model.py --random_seed 1 --init_method random --augment 6 --skip_connection no
+# python run_model.py --random_seed 1 --init_method random --augment 7 --skip_connection no
+# python run_model.py --random_seed 99 --init_method random --augment 1 --skip_connection no #redundant
+python run_model.py --random_seed 99 --init_method random --augment 2 --skip_connection no
+python run_model.py --random_seed 99 --init_method random --augment 3 --skip_connection no
+python run_model.py --random_seed 99 --init_method random --augment 4 --skip_connection no
+python run_model.py --random_seed 99 --init_method random --augment 5 --skip_connection no
+python run_model.py --random_seed 99 --init_method random --augment 6 --skip_connection no
+python run_model.py --random_seed 99 --init_method random --augment 7 --skip_connection no
+# # skip connections
+# # python run_model.py --random_seed 42 --init_method random --augment 1 --skip_connection no #redundant
+# python run_model.py --random_seed 42 --init_method random --augment 1 --skip_connection yes
+# # python run_model.py --random_seed 1 --init_method random --augment 1 --skip_connection no #redundant
+# python run_model.py --random_seed 1 --init_method random --augment 1 --skip_connection yes
+# # python run_model.py --random_seed 99 --init_method random --augment 1 --skip_connection no #redundant
+# python run_model.py --random_seed 99 --init_method random --augment 1 --skip_connection yes
